1e732407556dd469a5eddd20113b3664c88deb0c
Suman Paul <look4suman@gmail.com>
2026-02-08 00:16:37 +0530

    added changes for the Catalog microservices

---
+++ src/Services/Catalog/CatalogAPI/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace CatalogAPI.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    // Since C# 8, nullable reference types exist. I know this will be null right now, but trust me, I will assign a non-null value before using it.
+    public string Name { get; set; } = default!;
+    // new() removes the need to separately initialize the property before use
+    public List<string> Category { get; set; } = new();
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public decimal Price { get; set; }
+}
---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,39 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+using MediatR;
+
+namespace CatalogAPI.Products.CreateProduct;
+
+// record - reference type that behaves like a value type. Different from struct. Immutable.
+// var p1 = new Point(1, 2);
+// var p2 = p1 with { X = 10 };
+
+// Console.WriteLine(p1.X); // 1
+// Console.WriteLine(p2.X); // 10
+// C# record ≈ Go *struct + immutability + value equality
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // create product entity from command object
+        var product = new Product()
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile
+        };
+
+        // save to db
+
+
+        // return CreateProductResult
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
